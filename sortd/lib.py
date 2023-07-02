abf9349347fe5b1de2b5371e589d903e23e7302b
IIIIII <git@bigly.dog>
2023-07-01 22:11:58 -0700

    mypy

--- sortd/lib.py
+++ sortd/lib.py
@@ -1,4 +1,4 @@
-from collections.abc import ByteString, Iterable, Mapping
+from collections.abc import Iterable, Mapping
 from locale import strxfrm
 from logging import StreamHandler, getLogger
 from typing import Any
@@ -10,7 +10,7 @@ log.addHandler(StreamHandler())
 def recur_sort(data: Any) -> Any:
     if isinstance(data, Mapping):
         return {k: recur_sort(data[k]) for k in sorted(data, key=strxfrm)}
-    elif isinstance(data, Iterable) and not isinstance(data, (str, ByteString)):
+    elif isinstance(data, Iterable) and not isinstance(data, (str, bytes, bytearray)):
         return tuple(recur_sort(el) for el in data)
     else:
         return data
